0ad21085ddb2599928c6652c7b2d18ba6859b3f2
lexwlai <lex.lai90@gmail.com>
2019-12-05 21:26:01 -0500

    Cleaning up

--- Histogram.R
+++ Histogram.R
@@ -1,7 +1,6 @@
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 head(outcome)
 
+outcome <- as.numeric(outcome[, 11])
 
-outcome[, 11] <- as.numeric(outcome[, 11])
-
-hist(outcome[, 11])
+hist(outcome)
